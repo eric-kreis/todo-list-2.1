2debfd46a4b3d7636582fc46042630102db9da40
Eric Kreis <erickreis793@gmail.com>
2021-07-10 00:44:44 -0300

    Resolvendo conflito dependencias

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import { GlobalStyle } from './styles/styledComponents'
 import './styles/bootstrap.min.css'
 import './styles/changingBootstrap.css';
-import { GlobalStyle } from './styles/styledComponents'
 
 function App() {
   return (
