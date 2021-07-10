6014783c51046e1e947a35f3f23294f5e17fcce2
Eric Kreis <erickreis793@gmail.com>
2021-07-10 00:53:24 -0300

    Resolvendo conflito nas dependencias

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
+import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import { GlobalStyle } from './styles/styledComponents'
 import './styles/bootstrap.min.css'
 import './styles/changingBootstrap.css';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <Switch>
-        <Route exact path="/"><HomePage /></Route>
-      </Switch>
-    </BrowserRouter>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <GlobalStyle />
+        <Switch>
+          <Route exact path="/"><HomePage /></Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
