5d6747462e360e4bbe114ff442dad14a6376b32e
Eric Kreis <erickreis793@gmail.com>
2021-08-16 16:40:17 -0300

    Iniciando mudanças antes de continuar com FireBase

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import Login from './pages/Login';
+// import Login from './pages/Login';
 import HomePage from './pages/Home';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route path="/list" component={ HomePage } />
-      <Route exact path="/" component={ Login } />
+      <Route path="/" component={ HomePage } />
+      {/* <Route exact path="/" component={ Login } /> */}
     </Switch>
   );
 }
--- src/firebase/api.js
+++ src/firebase/api.js
@@ -4,6 +4,18 @@ import 'firebase/auth';
 // import 'firebase/firestore';
 import fireBaseConfig from './fireBaseConfig';
 
-const firebaseApp = firebase.initialize(fireBaseConfig);
+firebase.initialize(fireBaseConfig);
 
-export default { firebase, firebaseApp };
+// export default {
+//   createUser: firebase.auth().createUserWithEmailAndPassword(email, password)
+//     .then((userCredential) => {
+//     // Signed in
+//       var user = userCredential.user;
+//       // ...
+//     })
+//     .catch((error) => {
+//       var errorCode = error.code;
+//       var errorMessage = error.message;
+//       // ..
+//     }),
+// };
